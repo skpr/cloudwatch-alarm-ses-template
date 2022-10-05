9a99e259ddfd021c60299b6d4db5165f38016859
Nick Schuch <nick@previousnext.com.au>
2022-10-05 11:43:08 +1000

    Allow sending to multiple addresses

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,44 +20,45 @@ func main() {
 
 // HandleEvents will handle the SNS event.
 func HandleEvents(ctx context.Context, snsEvent events.SNSEvent) error {
-
 	// Authentication
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to setup client: %d", err)
 	}
+
 	sesclient := ses.NewFromConfig(cfg)
 
 	// Get SES configuration from lambda env variables
-	EmailTemplate := os.Getenv("SES_TEMPLATE")
-	SourceEmail := os.Getenv("EMAIL_FROM_ADDRESS")
-	DestinationToEmail := os.Getenv("EMAIL_TO_ADDRESSES")
-	DestinationCcEmail := os.Getenv("EMAIL_CC_ADDRESSES")
-	ReplyToEmail := os.Getenv("EMAIL_REPLY_TO_ADRESSES")
+	var (
+		emailTemplate = os.Getenv("SES_TEMPLATE")
+		fromAddress   = os.Getenv("EMAIL_FROM_ADDRESS")
+		toAddresses   = os.Getenv("EMAIL_TO_ADDRESSES")
+		ccAddresses   = os.Getenv("EMAIL_CC_ADDRESSES")
+		replyToEmail  = os.Getenv("EMAIL_REPLY_TO_ADRESSES")
+	)
 
-	emailDetails := &ses.SendTemplatedEmailInput{
+	input := &ses.SendTemplatedEmailInput{
 		Destination: &types.Destination{
-			ToAddresses: []string{DestinationToEmail},
+			ToAddresses: strings.Split(toAddresses, ","),
 		},
-		Source:           aws.String(SourceEmail),
-		Template:         aws.String(EmailTemplate),
+		Source:           aws.String(fromAddress),
+		Template:         aws.String(emailTemplate),
 		TemplateData:     aws.String(snsEvent.Records[0].SNS.Message),
-		ReplyToAddresses: []string{ReplyToEmail},
+		ReplyToAddresses: []string{replyToEmail},
 	}
 
 	// CC should be optional.
-	if DestinationCcEmail != "" {
-		emailDetails.Destination.CcAddresses = []string{DestinationCcEmail}
+	if ccAddresses != "" {
+		input.Destination.CcAddresses = strings.Split(ccAddresses, ",")
 	}
 
 	// Send Email.
-	resp, err := sesclient.SendTemplatedEmail(context.TODO(), emailDetails)
-
-	// Report errors if needed.
+	resp, err := sesclient.SendTemplatedEmail(ctx, input)
 	if err != nil {
-		return fmt.Errorf("could not send SES templated email to '%s': %s\n", SourceEmail, err.Error())
+		return fmt.Errorf("could not send SES templated email to: %w\n", err)
 	}
 
-	fmt.Printf("Email notification %s successfully sent to %s from %s\n", *resp.MessageId, DestinationToEmail, SourceEmail)
+	fmt.Printf("Email notification %s successfully sent to %d addresses\n", *resp.MessageId, len(input.Destination.ToAddresses))
+
 	return nil
 }
